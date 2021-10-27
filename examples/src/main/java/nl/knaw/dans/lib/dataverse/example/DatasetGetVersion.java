745a0d44a25cbd0e36181fe49f606e2b390fc176
Jan van Mansum <janvanmansum@gmail.com>
2021-10-27 15:26:52 +0200

    Extra logging in example

--- examples/src/main/java/nl/knaw/dans/lib/dataverse/example/DatasetGetVersion.java
+++ examples/src/main/java/nl/knaw/dans/lib/dataverse/example/DatasetGetVersion.java
@@ -29,6 +29,7 @@ public class DatasetGetVersion extends ExampleBase {
 
     public static void main(String[] args) throws Exception {
         DataverseResponse<List<DatasetVersion>> r = client.dataset(args[0]).getVersion(":latest");
+        log.info("Response message: {}", r.getEnvelopeAsJson().toPrettyString());
         log.info("Create Time: {}", r.getData().get(0).getCreateTime());
         log.info("Version State: {}", r.getData().get(0).getVersionState());
     }
