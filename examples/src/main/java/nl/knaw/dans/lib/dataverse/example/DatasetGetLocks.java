823160919a296b4bb5940480cf1224da351e476a
Vesa Akerman <vesa.akerman@dans.knaw.nl>
2021-11-19 15:51:46 +0100

    Added datasetGetLocks example program to test locking and releasing lock

---
+++ examples/src/main/java/nl/knaw/dans/lib/dataverse/example/DatasetGetLocks.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2021 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.lib.dataverse.example;
+
+import nl.knaw.dans.lib.dataverse.DataverseResponse;
+import nl.knaw.dans.lib.dataverse.ExampleBase;
+import nl.knaw.dans.lib.dataverse.model.workflow.Lock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class DatasetGetLocks extends ExampleBase {
+
+    private static final Logger log = LoggerFactory.getLogger(DatasetGetLocks.class);
+
+    public static void main(String[] args) throws Exception {
+        /*
+         * Start the example and then do something with the dataset that causes a lock, such as ingesting a
+         * tabular file.
+         */
+
+        String persistentId = args[0];
+        int awaitLockStateMaxNumberOfRetries = Integer.parseInt(args[1]);
+        int awaitLockStateMillisecondsBetweenRetries = Integer.parseInt(args[2]);
+        client.dataset(persistentId).awaitUnlock(awaitLockStateMaxNumberOfRetries, awaitLockStateMillisecondsBetweenRetries);
+
+        for (int i = 0; i < 300; i += 1) {
+            DataverseResponse<List<Lock>> response = client.dataset(persistentId).getLocks();
+            List<Lock> locks = response.getData();
+            log.trace(String.format("Locks: %s", locks));
+            if (!locks.isEmpty())
+                log.info(String.format("Dataset is currently locked by: %s", locks));
+            Thread.sleep(50);
+        }
+    }
+}
