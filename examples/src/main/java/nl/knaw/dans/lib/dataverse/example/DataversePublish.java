9a0e2fcc2841a4bf4d1e825f63cf66cbd29a7523
vesaakerman <vesa.akerman@dans.knaw.nl>
2021-11-19 16:05:25 +0100

    DD-680: dans-dataverse-client-lib: implement DataverseApi.publish() (#11)
    * Added DataverseApi.publish() and Dataset.publish() methods
    
    * corrected return types of the methods
    
    * check exception when trying to create a dataverse
    
    * removed creation of test dataverse instance

---
+++ examples/src/main/java/nl/knaw/dans/lib/dataverse/example/DataversePublish.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.lib.dataverse.example;
+
+import nl.knaw.dans.lib.dataverse.DataverseException;
+import nl.knaw.dans.lib.dataverse.DataverseHttpResponse;
+import nl.knaw.dans.lib.dataverse.ExampleBase;
+import nl.knaw.dans.lib.dataverse.model.DataMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataversePublish extends ExampleBase {
+
+    private static final Logger log = LoggerFactory.getLogger(DataversePublish.class);
+
+    public static void main(String[] args) throws Exception {
+        // Notice: the test dataverse instance has to exist before calling this method
+        log.info("--- BEGIN JSON OBJECT ---");
+        DataverseHttpResponse<DataMessage> r = client.dataverse("test").publish();
+        log.info("--- END JSON OBJECT ---");
+        log.info("Status Line of DATAVERSE PUBLICATION: {}", r.getHttpResponse().getStatusLine());
+    }
+}
--- lib/src/main/java/nl/knaw/dans/lib/dataverse/model/dataset/DatasetVersion.java
+++ lib/src/main/java/nl/knaw/dans/lib/dataverse/model/dataset/DatasetVersion.java
@@ -33,6 +33,8 @@ public class DatasetVersion {
     private String lastUpdateTime; // TODO: timestamp?
     private String releaseTime; // TODO: timestamp?
     private String createTime; // TODO: timestamp?
+    private String distributionDate; // TODO: timestamp?
+    private String productionDate; // TODO: timestamp?
     private boolean fileAccessRequest;
     private String termsOfUse;
     private String termsOfAccess;
@@ -129,6 +131,22 @@ public class DatasetVersion {
 
     public void setCreateTime(String createTime) {
         this.createTime = createTime;
+    }
+
+    public String getDistributionDate() {
+        return distributionDate;
+    }
+
+    public void setDistributionDate(String distributionDate) {
+        this.distributionDate = distributionDate;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
     }
 
     public boolean isFileAccessRequest() {
